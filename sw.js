511fc865797a91cd6a5ea25acb8cde8558bb3adb
toumaotoumiao <yumuyumumi@proton.me>
2026-09-05 18:43:33 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX="tomato-pl-";
-const CACHE_NAME=`${CACHE_PREFIX}v8.1.5.1`;
+const CACHE_NAME=`${CACHE_PREFIX}v8.1.5.3`;
 const APP_SHELL=["./","./index.html","./module-tools.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install",event=>{
