8e888392f257f55a17e0547d182651ff907966f7
toumaotoumiao <yumuyumumi@proton.me>
2026-09-12 17:11:22 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX="tomato-pl-";
-const CACHE_NAME=`${CACHE_PREFIX}v8.1.11.9`;
+const CACHE_NAME=`${CACHE_PREFIX}v8.1.11.13`;
 const RUNTIME_DOWNLOAD_CACHE="tomato-pl-runtime-downloads-v1";
 const APP_SHELL=["./","./index.html","./module-tools.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 
